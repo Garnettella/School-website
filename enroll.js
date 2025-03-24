ace366262aa230a71d7c993e4a50277a1edc4dcb
Garnettella <dthomasangella@gmail.com>
2025-03-24 11:45:29 +0100

    Add files via upload

---
+++ enroll.js
@@ -0,0 +1,10 @@
+    <script>
+            let popup = document.getElementById("popup");
+
+            function openPopup(){
+                popup.classList.add("open-popup")
+            }
+            function closePopup(){
+                popup.classList.remove("open-popup")
+            }
+        </script>
